55419cc268950aec1425085ca465a1fed12d7e10
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2021-09-26 16:11:32 +0530

    Completed

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const menucheck = document.querySelector(".tab2");
 const passcheck = document.querySelector(".container1");
 const menuvalid = document.querySelector(".tab3");
 const passvalid = document.querySelector(".container2");
+const menugen1 = document.querySelector(".tab11");
+const menucheck1 = document.querySelector(".tab22");
+const menuvalid1 = document.querySelector(".tab33");
 
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
@@ -107,3 +110,150 @@ menuvalid.addEventListener('click', ()=>{
     passcheck.classList.add('visible1');
     passvalid.classList.remove('visible1');
 })
+
+menugen1.addEventListener('click', ()=>{
+    passgen.classList.remove('visible1');
+    passcheck.classList.add('visible1');
+    passvalid.classList.add('visible1');
+	menuTray.classList.toggle('visible');
+})
+menucheck1.addEventListener('click', ()=>{
+    passgen.classList.add('visible1');
+    passcheck.classList.remove('visible1');
+    passvalid.classList.add('visible1');
+	menuTray.classList.toggle('visible');
+})
+menuvalid1.addEventListener('click', ()=>{
+    passgen.classList.add('visible1');
+    passcheck.classList.add('visible1');
+    passvalid.classList.remove('visible1');
+	menuTray.classList.toggle('visible');
+})
+
+//password strength checker
+
+const indicator = document.querySelector(".indicator");
+         const input = document.querySelector(".strength");
+         const weak = document.querySelector(".weak");
+         const medium = document.querySelector(".medium");
+         const strong = document.querySelector(".strong");
+         const text = document.querySelector(".text");
+         const showBtn = document.querySelector(".showBtn");
+         let regExpWeak = /[a-z]/;
+         let regExpMedium = /\d+/;
+         let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+         function trigger(){
+           if(input.value != ""){
+             indicator.style.display = "block";
+             indicator.style.display = "flex";
+             if(input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong)))no=1;
+             if(input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong))))no=2;
+             if(input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong))no=3;
+             if(no==1){
+               weak.classList.add("active");
+               text.style.display = "block";
+               text.textContent = "Your password is too weak";
+               text.classList.add("weak");
+             }
+             if(no==2){
+               medium.classList.add("active");
+               text.textContent = "Your password is intermediate";
+               text.classList.add("medium");
+             }else{
+               medium.classList.remove("active");
+               text.classList.remove("medium");
+             }
+             if(no==3){
+               weak.classList.add("active");
+               medium.classList.add("active");
+               strong.classList.add("active");
+               text.textContent = "Your password is strong";
+               text.classList.add("strong");
+             }else{
+               strong.classList.remove("active");
+               text.classList.remove("strong");
+             }
+             showBtn.style.display = "block";
+             showBtn.onclick = function(){
+               if(input.type == "password"){
+                 input.type = "text";
+                 showBtn.textContent = "HIDE";
+                 showBtn.style.color = "#23ad5c";
+               }else{
+                 input.type = "password";
+                 showBtn.textContent = "SHOW";
+                 showBtn.style.color = "#000";
+               }
+             }
+           }else{
+             indicator.style.display = "none";
+             text.style.display = "none";
+             showBtn.style.display = "none";
+           }
+         }
+
+//validator
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password field cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password field cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
